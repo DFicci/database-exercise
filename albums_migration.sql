dd3177f875e519485637642143c471050c441e80
Dficci <danielficci@gmail.com>
2022-03-28 14:40:32 -0500

    added albums_migrations.sql

---
+++ albums_migration.sql
@@ -0,0 +1,16 @@
+USE codeup_test_db;
+
+insert into `albums` (`artist`, `album_name`, `release_date`, `sales, genre`)
+values('Daft Punk', 'Homework', 1997, .220000, 'techno, house, French house');
+
+insert into `albums` (`artist`, `album_name`, `release_date`, `sales`, `genre`)
+values('Tiesto', 'Just Be', 2004, .073000, 'trance');
+
+insert into `albums` (`artist`, `album_name`, `release_date`, `sales`, `genre`)
+values('DeadMau5', 'Random Album Title', 2008, .072000, 'progressive house, electro house, ambient house');
+
+insert into `albums` (`artist`, `album_name`, `release_date`, `sales`, `genre`)
+values('Avicii', 'True', 2011, 5, 'Dance/Electronic, EDM, country, house, soul, bluegrass');
+
+insert into `albums` (`artist`, `album_name`, `release_date`, `sales`, `genre`)
+values('Avicii', 'The Days/Nights EP', 2014, 2.8, 'House, folktronica,electronic dance');
